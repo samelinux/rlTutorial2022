a0b0e5f7cde5f67687534ae00e1767b30c4b3693
Luca Giacometti <samelinux@gmail.com>
2022-06-28 19:37:58 +0200

    added some comments to main.c

--- main.c
+++ main.c
@@ -4,30 +4,41 @@
 //int main(int argc,char** argv)
 int main(void)
 {
+ //declare a player
  player_t player;
+ //declare a variable to store user input
+ char command=0;
 
+ //setup the "engine"
  signalInit();
  keyboardInit();
  screenInit();
 
+ //setup the player
  playerInit(&player);
 
- //just an example: roll some dice, draw some colored '#' and get user input
- char command=0;
+ //while the user has not pressed 'q'
  while(command!='q')
  {
+  //clear the screen
   screenClear();
 
+  //draw the player and a hint on how to quit
   screenPut(player.x,player.y,'@');
   screenPrint(0,screenHeight,"Press 'q' to quit");
 
+  //get player input
   command=keyboardRead();
+  //handle the input based on game/player status
   playerHandleInput(&player,command);
  }
 
+ //deinit the "engine"
  screenDeinit();
  keyboardDeinit();
  signalDeinit();
+
+ //return
  return 0;
 }
 
